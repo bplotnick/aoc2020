231eaa04d03bf27df3bcb30305d00edb2ed23364
Ben Plotnick <ben.plotnick@gmail.com>
2020-12-03 01:53:04 -0800

    Day 3 done

---
+++ day3/part1.py
@@ -0,0 +1,18 @@
+import numpy as np
+filename = 'input.txt'
+
+move_x = 3
+move_y = 1
+
+def main():
+    with open(filename) as f:
+        arr = np.array([list(l.strip()) for l in f])
+
+    # assumes that numcols < numrows
+    idx_row = [row for row in range(arr.shape[0])]
+    idx_col = [(row*move_x) % arr.shape[1] for row in range(arr.shape[0])]
+
+    print((arr[idx_row, idx_col] == '#').sum())
+
+if __name__ == '__main__':
+    main()
---
+++ day3/part2.py
@@ -0,0 +1,35 @@
+import logging
+import numpy as np
+filename = 'input.txt'
+
+def sol(arr: np.ndarray, move_x: int, move_y: int) -> int:
+    num_indices = max([arr.shape[0]//move_y, arr.shape[1]//move_x])
+    idx_row = [(i*move_y) % arr.shape[0] for i in range(num_indices)]
+    idx_col = [(i*move_x) % arr.shape[1] for i in range(num_indices)]
+    logging.debug(idx_row)
+    logging.debug(idx_col)
+    
+    return (arr[idx_row, idx_col] == '#').sum()
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    with open(filename) as f:
+        arr = np.array([list(l.strip()) for l in f])
+
+    inputs = [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ]
+    total = 1
+    for i in inputs:
+        s = sol(arr, i[0], i[1])
+        logging.info(f'Input {i}; sol {s}')
+        total *= s
+    print(total)
+
+
+if __name__ == '__main__':
+    main()
